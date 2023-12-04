faef1c49c5c957ac53ab3d42fc70040d6ee6c10f
도경 <dokyungkim2926@gmail.com>
2023-12-04 19:48:16 +0900

    테이블 구축

---
+++ src/main/kotlin/com/example/teentime/domain/user/domain/User.kt
@@ -0,0 +1,42 @@
+package com.example.teentime.domain.user.domain
+
+import com.example.teentime.global.entity.BaseUUIDEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import java.util.UUID
+
+@Entity(name = "tbl_user")
+class User(
+    id: UUID?,
+
+    @Column(name = "accountId", nullable = false, unique = true)
+    val accountId: String,
+
+    @Column(name = "name", nullable = false)
+    val name: String,
+
+    @Column(name = "phone_number" ,nullable = false, columnDefinition = "CHAR(11)")
+    val phoneNumber: String,
+
+    @Column(name = "gender", nullable = false)
+    val gender: String,
+
+    @Column(name = "birth_year", nullable = false)
+    val birthYear: String,
+
+    @Column(name = "birthday", nullable = false)
+    val birthday: String,
+
+    @Column(name = "email", nullable = true)
+    val email: String,
+
+    @Column(name = "school_name")
+    val schoolName: String,
+
+    @Column(name = "school_major")
+    val schoolMajor: String,
+
+    @Column(name = "grade")
+    val grade: String
+
+): BaseUUIDEntity(id)
