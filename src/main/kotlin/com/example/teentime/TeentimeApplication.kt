a104e6959d7fb6f300cf81eedc88dfcf98b0b83f
도경 <dokyungkim2926@gmail.com>
2023-12-04 17:25:21 +0900

    commit

---
+++ src/main/kotlin/com/example/teentime/TeentimeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.teentime
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TeentimeApplication
+
+fun main(args: Array<String>) {
+    runApplication<TeentimeApplication>(*args)
+}
